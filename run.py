2d76b8745295fea0b1c5adf7b7e94b2785095dd6
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-08-17 20:59:27 +0200

    Wire up APIs and confirm to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,17 @@
-print("hai")
+import curses
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive",
+]
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("slithering_challenge")
+easy = SHEET.worksheet("easy")
+
+data = easy.get_all_values()
+print(data)
