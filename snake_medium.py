2042cf67aa62ff74d8f95d8802d23048200e752d
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-08-17 23:26:43 +0200

    Add main function

--- snake_medium.py
+++ snake_medium.py
@@ -3,3 +3,12 @@ from random import randint
 from time import sleep
 import gspread
 from google.oauth2.service_account import Credentials
+
+
+def main(stdscr):
+    """
+    main function that controls the game. It receives a stdscr object which represents the game screen.
+    """
+    stdscr.clear()
+    sh = 20
+    sw = 60
