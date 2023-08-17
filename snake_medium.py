206cfe65df8b26e3ede4235797ab4fd6ef0a57bf
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-08-17 23:35:42 +0200

    Add code for collision detection

--- snake_medium.py
+++ snake_medium.py
@@ -18,3 +18,61 @@ def main(stdscr):
     curses.curs_set(0)
     win.border(0)
     win.nodelay(1)
+    snake = [(sh // 2, sw // 2), (sh // 2, sw // 2 - 1), (sh // 2, sw // 2 - 2)]
+    food = ()
+    obstacles = [
+        (sh // 2 - 3, sw // 2),
+        (sh // 2 + 3, sw // 2),
+        (sh // 2, sw // 2 - 10),
+    ]
+    ESC = 27
+    key = curses.KEY_RIGHT
+    prev_key = key
+
+    score = 0
+    lives = 3
+
+    while key != ESC and lives > 0:
+        win.addnstr(0, 2, "Score: " + str(score) + " ", 20)
+        win.addnstr(0, sw - 10, "Lives: " + str(lives) + " ", 20)
+        win.timeout(150 - (len(snake)) // 5 + len(snake) // 10 % 120)
+        event = win.getch()
+
+        if event != -1:
+            if event == curses.KEY_DOWN and prev_key != curses.KEY_UP:
+                key = curses.KEY_DOWN
+            if event == curses.KEY_UP and prev_key != curses.KEY_DOWN:
+                key = curses.KEY_UP
+            if event == curses.KEY_LEFT and prev_key != curses.KEY_RIGHT:
+                key = curses.KEY_LEFT
+            if event == curses.KEY_RIGHT and prev_key != curses.KEY_LEFT:
+                key = curses.KEY_RIGHT
+
+        prev_key = key
+
+        y, x = snake[0]
+
+        if key == curses.KEY_DOWN and prev_key != curses.KEY_UP:
+            y += 1
+        if key == curses.KEY_UP and prev_key != curses.KEY_DOWN:
+            y -= 1
+        if key == curses.KEY_LEFT and prev_key != curses.KEY_RIGHT:
+            x -= 1
+        if key == curses.KEY_RIGHT and prev_key != curses.KEY_LEFT:
+            x += 1
+
+        if y == 0 or y == sh or x == 0 or x == sw:
+            lives -= 1
+            if lives == 0:
+                break
+            else:
+                sleep(1)
+                continue
+
+        if (y, x) in snake[1:]:
+            lives -= 1
+            if lives == 0:
+                break
+            else:
+                sleep(1)
+                continue
