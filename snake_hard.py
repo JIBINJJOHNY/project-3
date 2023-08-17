b7d3f9beea485def4a0b993d388d7a29f9c7355d
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-08-17 23:58:41 +0200

    Add timer 2min for game over

--- snake_hard.py
+++ snake_hard.py
@@ -3,3 +3,38 @@ from random import randint
 from time import sleep
 import gspread
 from google.oauth2.service_account import Credentials
+
+
+def main(stdscr):
+    """
+    The main function is the entry point of the program.
+    It receives the stdscr object, which represents the game screen.
+    """
+    stdscr.clear()
+    sh = 20
+    sw = 60
+
+    win = curses.newwin(sh + 1, sw + 1, 0, 0)
+    win.keypad(1)
+    curses.noecho()
+    curses.curs_set(0)
+    win.border(0)
+    win.nodelay(1)
+
+    snake = [(sh // 2, sw // 2), (sh // 2, sw // 2 - 1), (sh // 2, sw // 2 - 2)]
+    food = ()
+    obstacles = [
+        (sh // 2 - 3, sw // 2),
+        (sh // 2 + 3, sw // 2),
+        (sh // 2, sw // 2 - 10),
+        (sh // 2 - 5, sw // 2 + 5),
+        (sh // 2 + 5, sw // 2 - 7),
+    ]
+    ESC = 27
+    key = curses.KEY_RIGHT
+    prev_key = key
+
+    score = 0
+    lives = 3
+    timer_start = time.time()  # Timer start time
+    timer_duration = 120  # 2 minutes in seconds
