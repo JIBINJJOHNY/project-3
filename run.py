d058c493d2f3f304aca69c582b2b85f2f63e13e0
JIBIN JAIN JOHNY <jibinjjohny11@gmail.com>
2023-08-17 21:11:49 +0200

    Add show menu method displays the main menu and stores the chosen menu option's index in current_state

--- run.py
+++ run.py
@@ -1,17 +1,30 @@
 import curses
+from simple_term_menu import TerminalMenu
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive",
-]
-CREDS = Credentials.from_service_account_file("creds.json")
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("slithering_challenge")
-easy = SHEET.worksheet("easy")
 
-data = easy.get_all_values()
-print(data)
+class GameMenu:
+    def __init__(self):
+        # Initialize the main menu using TerminalMenu
+        self.menu = TerminalMenu(["Start Game", "Instructions", "Leaderboard", "Quit"])
+        # OAuth2 scopes for Google Sheets and Drive API
+        self.SCOPE = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive",
+        ]
+        self.CREDS = Credentials.from_service_account_file("creds.json")
+        self.SCOPED_CREDS = self.CREDS.with_scopes(self.SCOPE)
+        self.GSPREAD_CLIENT = gspread.authorize(self.SCOPED_CREDS)
+        self.SHEET = self.GSPREAD_CLIENT.open("slithering_challenge")
+        self.current_state = None
+
+    def show_menu(self):
+        """
+        show_menu method displays the main menu and stores the chosen menu option's index in current_state
+
+        """
+        print("Welcome to Slithering Challenge\n")  # Heading
+        menu_entry_index = self.menu.show()
+        self.current_state = menu_entry_index
